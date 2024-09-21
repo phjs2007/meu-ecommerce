e97bcd104d15013351549d73ae958d0facc3a624
phjs2007 <phjs7626@gmail.com>
2024-09-21 16:40:37 -0300

    atu 2
    novas fotos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    < div >
+    < div className="bg-[url('/imagens/back.jpg)]">
       <Header />
+      <section>
+        <div>
+          <div>
+          <Image
+                            className=" "
+                            src="/imagens/Frostpunk2.webp"
+                            alt="Logo da Loja"
+                            width={400}
+                            height={200}
+                            priority
+                            layout="fixed"/>
+          </div>
+        </div>
+      </section>
     </div >
   );
 }
